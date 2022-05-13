a186647966ba8a2e7999f83cce398840ffbe7572
zhaosiqi <zsqandzyr@qq.com>
2022-05-13 18:15:26 +0800

    JNI提交

---
+++ app/src/main/java/com/able/halmanager/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.able.halmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.able.halmanager.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sampleText.text = stringFromJNI()
+    }
+    external fun stringFromJNI(): String
+    companion object {
+        init {
+            System.loadLibrary("halmanager")
+        }
+    }
+}
