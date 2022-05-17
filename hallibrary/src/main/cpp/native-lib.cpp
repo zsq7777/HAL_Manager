f133c595ce30a49c4572cab7157e6a35adc088b1
zhaosiqi <zsqandzyr@qq.com>
2022-05-17 17:24:25 +0800

    增加library，ndk移至library中

--- hallibrary/src/main/cpp/native-lib.cpp
+++ hallibrary/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_able_halmanager_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "C++";
     return env->NewStringUTF(hello.c_str());
 }
